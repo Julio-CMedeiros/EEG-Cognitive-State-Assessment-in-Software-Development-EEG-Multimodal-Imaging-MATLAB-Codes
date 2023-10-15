45290af5bb581ed121a64e78d233e123d409cf2f
Júlio Medeiros <93949989+Julio-CMedeiros@users.noreply.github.com>
2023-10-15 01:02:56 +0100

    Update and rename NeuroElfAnalysis.m to NeuroElf_CorrelationAnalysis.m

--- NeuroElf_CorrelationAnalysis.m
+++ NeuroElf_CorrelationAnalysis.m
@@ -1,5 +1,5 @@
 % ----------------------------------------------
-% Script Name: VTC_CorrelationAnalysis.m
+% Script Name: NeuroEl_CorrelationAnalysis.m
 % Author: Julio Medeiros
 % Email: juliomedeiros@dei.uc.pt
 % Institution: University of Coimbra (UC), Centre for Informatics and Systems of the University of Coimbra (UC)
